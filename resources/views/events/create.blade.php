874fdd5f26f2b0b491df9c1e1419fa9acef99426
Kaio Rodrigo <kaiorodrigocoder@gmail.com>
2022-01-13 21:54:24 -0300

    Pagina Home pronta

--- resources/views/events/create.blade.php
+++ resources/views/events/create.blade.php
@@ -4,6 +4,34 @@
 
 @section('content')
 
-  <h1>Crie um evento</h1>
+<div id="event-create-container" class="col-md-6 offset-md-3">
+
+<h1>Crie o seu evento</h1>
+
+<form action="/events" method="POST">
+  <div class="form-group">
+    <label for="tite">Evento: </label>
+    <input type="text" class="form-control" name="title" id="title" placeholder="Nome do evento">
+  </div>
+  <div class="form-group">
+    <label for="tite">Cidade: </label>
+    <input type="text" class="form-control" name="city" id="city" placeholder="Local do evento">
+  </div>
+  <div class="form-group">
+    <label for="tite">O evento é privado ? </label>
+    <select name="private" id="private" class="form-control">
+      <option value="0">Não</option>
+      <option value="1">Sim</option>
+    </select>
+  </div>
+  <div class="form-group">
+    <label for="tite">Descrição </label>
+    <textarea name="description" id="description" class="form-control" placeholder="O que vai acontecer no evento ?"></textarea>
+  </div>
+  <input type="submit" class="btn btn-primary" value="Criar Evento">
+</form>
+
+</div>
+  
 
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -19,8 +19,5 @@ Route::get('/', [EventController::class, 'index']);
 Route::get('/events/create', [EventController::class, 'create']);
 
 Route::get('/contact', function(){
-
-    
-
     return view('contact');
 });
