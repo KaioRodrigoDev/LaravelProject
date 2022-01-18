5161c79059e2ff127c5dcea12bff273937f4b91f
Kaio Rodrigo <kaiorodrigocoder@gmail.com>
2022-01-18 10:25:43 -0300

    Resgatando registro do Banco Incompleto

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -25,7 +25,7 @@
         <p class='card-date'>02/06/2003</p>
         <h5 class="card-title">{{$event->title}}</h6>
         <p class="card-participants">X participantes</p>
-        <a href="#" class="btn btn-primary">Saber mais</a>
+        <a href="/events/{{ $event->$id }}" class="btn btn-primary">Saber mais</a>
       </div>
     </div>
 
