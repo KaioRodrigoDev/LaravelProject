c76df77a9f0a09bb39c76b3a891ad4d0e6b3c414
Kaio Rodrigo <kaiorodrigocoder@gmail.com>
2022-01-27 14:52:18 -0300

    Busca no Laravel

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,14 +7,19 @@
 <div id="search-container" class="col-md-12">
 
   <h1> Busque um evento</h1>
-  <form action="">
-    <input type="text" id="search" class="form-control" placeholder = 'Procurar ...'>
+  <form action="/" mothod='GET'>
+    <input type="text" id="search" name='search' class="form-control" placeholder = 'Procurar ...'>
   </form>
 
 </div>
   
 <div id="events-container" class="col-md-12">
+  @if ($search)
+      <h2>Buscando por: {{$search}}</h2>
+  @else
   <h2>Proximos eventos</h2>
+  @endif
+  
   <p class="subtitle">Veja os eventos dos proximos dias</p>
   <div id="cards-container" class="row">
     @foreach($events as $event)
@@ -30,9 +35,10 @@
     </div>
 
     @endforeach
-    @if (count($events) == 0)
+    @if (count($events) == 0 && $search)
+      <p>Nenhum evento em relação a: {{$search}} !<a href="/">Ver todos</a></p>
+    @elseif(count($events) == 0)
       <p>Não ha eventos disponiveis</p>
-
     @endif
   </div>
 </div>
