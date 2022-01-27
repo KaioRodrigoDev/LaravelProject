0cf951a5770b6509e5ed24bf5944b7a1a2c609bf
Kaio Rodrigo <kaiorodrigocoder@gmail.com>
2022-01-27 12:37:49 -0300

    Salvando JSON no banco

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -27,6 +27,7 @@ class EventController extends Controller
         $event->city = $request->city;
         $event->description = $request->description;
         $event->private = $request->private;
+        $event->items = $request->items;
 
         //Image
         if($request->hasFile('image') && $request->file('image')->isValid()){
